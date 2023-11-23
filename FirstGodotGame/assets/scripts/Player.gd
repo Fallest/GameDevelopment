356f9db0f957e3defb54e5cba175fbaef4075686
David <56403805+Fallest@users.noreply.github.com>
2023-11-23 14:21:17 +0100

    Created first Godot game (#2)

---
+++ FirstGodotGame/assets/scripts/Player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("what")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = direction  * 200
+	move_and_slide()
+
